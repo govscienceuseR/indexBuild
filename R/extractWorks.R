7b962cae05d353f4e2c90558057128dd4ef94ff6
Tyler Scott <tascott@ucdavis.edu>
2023-09-21 09:20:00 -0700

    convert venue to source

--- R/extractWorks.R
+++ R/extractWorks.R
@@ -14,8 +14,8 @@
 #' @param dest_file location to save output as a json.gz
 #' @param return boolean for whether final result should be returned as workspace object
 #' @param reduce boolean for whether to reduce scope of final results, see @details
-#' @param venue_id (optional) openAlex ID# for the venue associated with the work(s)
-#' @param venue_page (optional) openAlex webpage for the venue
+#' @param source_id (optional) openAlex ID# for the source associated with the work(s)
+#' @param source_page (optional) openAlex webpage for the source
 #' @param override override 1M query result limit?
 #' @description Primary use is to query a concept ID and extract associated works, e.g., all article records associated with "habitat"
 #' @details Note that because extracted records can be pretty large, there is an optional "reduce" command that selects out a subset of key variables before saving or returning the final json list
@@ -24,10 +24,11 @@
 #' @import httr
 #' @import stringr
 
-extractWorks <- function(dest_file = NULL,override = F,mailto = NULL,concept_id = NULL,concept_page = NULL,venue_id = NULL,venue_page = NULL,cursor = T,per_page = NULL,to_date = NULL,from_date = NULL,keep_paratext = FALSE,debug = FALSE,sleep_time = 0.1,reduce = T,return = T){
+extractWorks <- function(dest_file = NULL,override = F,mailto = NULL,concept_id = NULL,concept_page = NULL,source_id = NULL,source_page = NULL,cursor = T,per_page = NULL,to_date = NULL,from_date = NULL,keep_paratext = FALSE,debug = FALSE,sleep_time = 0.1,reduce = T,return = T){
   if(missing(concept_id)&!missing(concept_page)){concept_id <- stringr::str_extract(concept_page,'[A-Za-z0-9]+$')}
-  if(missing(venue_id)&!missing(venue_page)){venue_id <- stringr::str_extract(venue_page,'[A-Za-z0-9]+$')}
-  if(missing(venue_id)&missing(concept_id)){stop("Must specify a concept and/or a venue to query (using page or id)")}
+  if(missing(source_id)&!missing(source_page)){source_id <- stringr::str_extract(source_page,'[A-Za-z0-9]+$')}
+  if(missing(source_id)&missing(concept_id)){stop("Must specify a concept and/or a source to query (using page or id)")}
+  if(missing(dest_file)&return==F){stop("Must specify a file destination to save the result or set return = T")}
   works_base <- 'https://api.openalex.org/works'
   url <- parse_url(works_base)
   if(!is.null(mailto)){
@@ -37,13 +38,14 @@ extractWorks <- function(dest_file = NULL,override = F,mailto = NULL,concept_id 
     url$query$cursor<-"*"
     }
   if(!is.null(per_page)){
+    if(per_page>200){per_page = 200;print('openAlex allows up to 200 results per page, lowering to per_page = 200')}
     url$query$`per-page`<-per_page
     }
   if(!missing(concept_id)){
     url$query$filter$concept.id<-concept_id
     }
-  if(!missing(venue_id)){
-    url$query$filter$host_venue.id<-venue_id
+  if(!missing(source_id)){
+    url$query$filter$locations.source.id<-source_id
     }
   if(!missing(from_date)){
     from_date <- if(nchar(from_date)==4){paste(from_date,'01','01',sep = '-')}
@@ -73,7 +75,7 @@ extractWorks <- function(dest_file = NULL,override = F,mailto = NULL,concept_id 
       }
       else{print(paste0(js$meta$count,' works found'))}
     }
-    reduce_vars <- c('id','title','doi','host_venue','cited_by_count','is_paratext','open_access','publication_year','authorships','type')
+    reduce_vars <- c('id','title','doi','source','cited_by_count','is_paratext','open_access','publication_year','authorships','type')
     if(reduce){
       js$results <- lapply(js$results,function(x) x[reduce_vars])
       }
@@ -84,7 +86,7 @@ extractWorks <- function(dest_file = NULL,override = F,mailto = NULL,concept_id 
     Sys.sleep(sleep_time)
   }
   json_object <- toJSON(temp_js_list)
-  if(!is.null(file)){
+  if(!missing(dest_file)){
     print(paste('saving result'))
     write(json_object, file=gzfile(dest_file))
   }
